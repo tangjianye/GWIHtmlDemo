e495e20aaafa39c70ab4bd1f6a6aa9c948bdd160
tangjianye <tangjianye000000@126.com>
2016-05-12 15:04:48 +0800

    优化图片包位置

--- app/src/main/java/com/product/htmldemo/MainActivity.java
+++ app/src/main/java/com/product/htmldemo/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppWebActivity {
         // loadUrl("file:///android_asset/htmlBlue/index.html");
         // loadUrl("file:///android_asset/htmlRed/index.html");
         loadUrl("file:///android_asset/index.html");
+
+        // loadUrl("file:///mnt/sdcard/html/index.html");
+        // loadUrl("content://com.android.htmlfileprovider/mnt/sdcard/html/index.html");
+        // loadUrl("content://com.android.htmlfileprovider/sdcard/html/index.html");
+        // loadUrl("content://com.android.htmlfileprovider" + Environment.getExternalStorageDirectory() + File.separator + "html" + File.separator + "index.html");
     }
 }
